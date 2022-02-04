78fc58e9ca590fbc74be53a5261d02e8352cc22b
Matthew Nguyen <contact@n-studio.fr>
2022-02-04 16:52:59 +0800

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,27 @@
-// swift-tools-version:5.5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    platforms: [
+        .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)
+    ],
+    products: [
+        .library(
+            name: "DateToolsSwift",
+            targets: ["DateToolsSwift"])
+    ],
     targets: [
-       .target(
-           name: "DateToolsSwift",
-           path: "DateToolsSwift/DateTools",
-           exclude: ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
+        .target(
+            name: "DateToolsSwift",
+            path: "DateToolsSwift/DateTools",
+            exclude: [
+                "Examples",
+                "DateToolsSwift/Examples"
+            ],
+            resources: [
+                .copy("DateTools.bundle")
+            ]
         )
     ]
 )
